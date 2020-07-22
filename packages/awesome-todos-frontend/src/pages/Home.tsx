33fab7d7f7f2ca0303d1d4c4dc2aeb69d135e5f1
Pedro Costa <pedro.costa@thetrainline.com>
2020-07-22 16:24:46 +0100

    Fix typo

--- packages/awesome-todos-frontend/src/pages/Home.tsx
+++ packages/awesome-todos-frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Dashboard from '../components/dashboard2/Dashboard';
+import Dashboard from '../components/dashboard/Dashboard';
 import Header from '../components/header/Header';
 
 const Home: React.FC = () => {
