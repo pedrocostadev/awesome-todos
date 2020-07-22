158d839eb7d86e75aa8f56af938f440d062dabd3
Pedro Costa <pedro.costa.dev@gmail.com>
2020-07-23 00:26:58 +0100

    Adds logo to header

--- packages/awesome-todos-frontend/src/components/header/Header.tsx
+++ packages/awesome-todos-frontend/src/components/header/Header.tsx
@@ -3,6 +3,7 @@ import './Header.css';
 
 const Header: React.FC = () => (
   <header className="header-container">
+    <img className="logo" src="./logo.png" alt="awesome-todos logo" />
     <span className="header-app-name">Awesome Todos App</span>
   </header>
 );
