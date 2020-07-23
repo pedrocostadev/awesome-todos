922a62499796471ddf8e9e24fee843616ec85d17
Pedro Costa <pedro.costa.dev@gmail.com>
2020-07-24 00:48:55 +0100

    Set api baseurl

--- packages/awesome-todos-frontend/src/services/awesomeTodosApiClient/apiClient.ts
+++ packages/awesome-todos-frontend/src/services/awesomeTodosApiClient/apiClient.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-console.log('process.env', process.env);
-
 const apiClient = axios.create({
-  baseURL: 'http://awesome-todos-20.herokuapp.com:8080/', // process.env.REACT_APP_API_URL || ':8080',
+  baseURL: 'http://127.0.0.1:8080/',
   withCredentials: true,
 });
 
