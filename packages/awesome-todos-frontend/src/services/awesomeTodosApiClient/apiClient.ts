d03ae4ea2eddee40b8e4ad6c5b8679bfb0350cda
Pedro Costa <pedro.costa.dev@gmail.com>
2020-07-24 00:40:00 +0100

    Deploy test

--- packages/awesome-todos-frontend/src/services/awesomeTodosApiClient/apiClient.ts
+++ packages/awesome-todos-frontend/src/services/awesomeTodosApiClient/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 console.log('process.env', process.env);
 
 const apiClient = axios.create({
-  baseURL: ':8080', // process.env.REACT_APP_API_URL || ':8080',
+  baseURL: 'http://awesome-todos-20.herokuapp.com:8080/', // process.env.REACT_APP_API_URL || ':8080',
   withCredentials: true,
 });
 
