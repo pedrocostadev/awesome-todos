031cf8e873e3ce731aeed64ac2822f2f381e9660
Pedro Costa <pedro.costa.dev@gmail.com>
2020-07-24 00:53:32 +0100

    Deploy test

--- packages/awesome-todos-backend/server.ts
+++ packages/awesome-todos-backend/server.ts
@@ -15,12 +15,9 @@ const app = express();
 
 const corsOrigin = [];
 
-if (process.env.NODE_ENV === 'dev') {
-  corsOrigin.push('http://192.168.1.104:3000');
-  corsOrigin.push('http://localhost:3000');
-} else {
-  corsOrigin.push(process.env.APP_URL);
-}
+corsOrigin.push('http://192.168.1.104:3000');
+corsOrigin.push('http://localhost:3000');
+corsOrigin.push(process.env.APP_URL);
 
 app.use(
   cors({
